08fb0305f0ffe2795351fe822a62c76f2fcd2f68
adityadaharwal963 <adityaprowork@gmail.com>
2026-01-21 23:07:10 +0530

    add mergeKSortedList

---
+++ src/week3/day3/Heap/src/LinkNode.java
@@ -0,0 +1,20 @@
+public class LinkNode<T>{
+    public T value;
+    public LinkNode<T> next;
+    public LinkNode<T> prev;
+
+    public LinkNode(T value) {
+        this.value = value;
+        this.next = null;
+        this.prev = null;
+    }
+
+    public T getValue() {
+        return this.value;
+    }
+
+    public LinkNode<T> getNext(){
+        return this.next;
+    }
+
+}
--- src/week3/day3/Heap/src/MergeKSortedList.java
+++ src/week3/day3/Heap/src/MergeKSortedList.java
@@ -1,13 +1,54 @@
-import java.util.List;
-import java.util.PriorityQueue;
+import java.util.*;
+
 
 public class MergeKSortedList {
-    static void mergeList(List<Integer>[] lists){
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for(List<Integer> list: lists){
-            for(Integer i : list){
-                pq.add()
-            }
+    static void mergeList(LinkList<Integer>[] lists){
+        PriorityQueue<LinkNode<Integer>> pq = new PriorityQueue<>(Comparator.comparing(LinkNode::getValue));
+        LinkList<Integer> newList = new LinkList<>();
+
+        for (LinkList<Integer> list : lists) {
+            pq.offer(list.getHead());
         }
+        //
+        while (!pq.isEmpty()){
+
+           LinkNode<Integer> curr =  pq.poll();
+           if(curr.getNext()!=null)
+               pq.offer(curr.getNext());
+           newList.add(curr.getValue());
+        }
+
+        newList.traverse();
+    }
+
+    public static void main(String[] args) {
+        LinkList<Integer> list1 = new LinkList<>();
+        LinkList<Integer> list2 = new LinkList<>();
+        LinkList<Integer> list3 = new LinkList<>();
+        LinkList<Integer> list4 = new LinkList<>();
+        LinkList<Integer>[] lists = new LinkList[]{list1, list2, list3, list4};;
+
+        list1.add(1);
+        list1.add(2);
+        list1.add(4);
+        list1.add(6);
+        list1.add(8);
+
+        list2.add(1);
+        list2.add(3);
+        list2.add(5);
+        list2.add(7);
+
+        list3.add(8);
+        list3.add(11);
+        list3.add(12);
+        list3.add(14);
+
+        list4.add(1);
+        list4.add(13);
+        list4.add(15);
+        list4.add(16);
+
+        mergeList(lists);
     }
 }
